a5b5a4af5b736b81853738636c4e250e8ad510ee
thomteam.t@gmail.com <thomteam.t@gmail.com>
2016-04-28 18:20:42 +0900

    database

--- db.sql
+++ db.sql
@@ -11,9 +11,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping structure for table dusal.articles
-DROP TABLE IF EXISTS `articles`;
 CREATE TABLE IF NOT EXISTS `articles` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `clicked` int(200) unsigned NOT NULL DEFAULT '0',
   `publisher_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
   `sub_id` int(10) unsigned NOT NULL DEFAULT '0',
   `active` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
@@ -29,37 +29,58 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table dusal.articles: ~4 rows (approximately)
+-- Dumping data for table dusal.articles: ~3 rows (approximately)
 /*!40000 ALTER TABLE `articles` DISABLE KEYS */;
-INSERT INTO `articles` (`id`, `publisher_name`, `sub_id`, `active`, `user_id`, `title`, `file`, `some_text`, `co_author`, `category_id`, `search_keys`, `publication_date`, `page_count`, `created_at`, `updated_at`) VALUES
-	(1, 'Батсүрэн', 9, '1', 1, 'Найк', '571b45eb95453asdasdasd.pdf', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.', 'asd', 1, 'kajsdkajshd', '1111-11-22', '22', '2016-04-23 17:52:43', '2016-04-24 18:26:29'),
-	(3, '0', 0, '1', 2, '0', '0', NULL, '0', 0, '0', '0000-00-00', '0', '0000-00-00 00:00:00', '2016-04-25 22:58:50'),
-	(4, '0', 0, '0', 1, '0', '0', NULL, '0', 0, '0', '0000-00-00', '0', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-	(5, '0', 0, '0', 1, '0', '0', NULL, '0', 0, '0', '0000-00-00', '0', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `articles` (`id`, `clicked`, `publisher_name`, `sub_id`, `active`, `user_id`, `title`, `file`, `some_text`, `co_author`, `category_id`, `search_keys`, `publication_date`, `page_count`, `created_at`, `updated_at`) VALUES
+	(1, 5, 'Батсүрэн', 9, '1', 1, 'Найк', '571b45eb95453asdasdasd.pdf', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.', 'asd', 1, 'kajsdkajshd', '1111-11-22', '22', '2016-04-23 17:52:43', '2016-04-28 08:37:10'),
+	(6, 11, 'Мөнхтулга', 2, '1', 8, 'Хуралын илтгэл', '5721aaacd814eCWYK8CLC61.jpg', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'Ганболд, Сүх-Очир', 3, 'Lorem Ipsum is simply dummy text of ', '2016-04-13', '20', '2016-04-28 06:16:12', '2016-04-28 09:05:25'),
+	(7, 0, 'Мөнхтулга', 2, '1', 8, 'Bootstrap Skeleton', '5721ab0b077f31C1A9389B4.jpg', 'asjkdh', 'akjshd', 2, 'kajsdkajshd', '2016-04-08', '22', '2016-04-28 06:17:47', '2016-04-28 06:17:47');
 /*!40000 ALTER TABLE `articles` ENABLE KEYS */;
 
 
 -- Dumping structure for table dusal.articles_category
-DROP TABLE IF EXISTS `articles_category`;
 CREATE TABLE IF NOT EXISTS `articles_category` (
-  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cat_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `category_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
-  PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  PRIMARY KEY (`cat_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table dusal.articles_category: ~2 rows (approximately)
+-- Dumping data for table dusal.articles_category: ~5 rows (approximately)
 /*!40000 ALTER TABLE `articles_category` DISABLE KEYS */;
-INSERT INTO `articles_category` (`category_id`, `category_name`) VALUES
-	(1, 'Эрдэм шинжилгээний бүтээл'),
-	(2, 'Эрдэм шинжилгээний хурлын илтгэл'),
-	(3, 'Лекц');
+INSERT INTO `articles_category` (`cat_id`, `category_name`) VALUES
+	(1, 'Математик'),
+	(2, 'Физик'),
+	(3, 'Хими'),
+	(4, 'Газарзүй Геологи'),
+	(5, 'ХАА Биологи');
 /*!40000 ALTER TABLE `articles_category` ENABLE KEYS */;
 
 
+-- Dumping structure for table dusal.events
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(225) CHARACTER SET utf8 NOT NULL,
+  `body` varchar(1000) CHARACTER SET utf8 NOT NULL,
+  `author` varchar(1000) CHARACTER SET utf8 NOT NULL,
+  `lector` varchar(1000) CHARACTER SET utf8 DEFAULT NULL,
+  `where_is` varchar(1000) CHARACTER SET utf8 NOT NULL,
+  `when_year` year(4) NOT NULL,
+  `when_month` int(2) NOT NULL,
+  `when_day` int(2) NOT NULL,
+  `when_time` time NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Dumping data for table dusal.events: 0 rows
+/*!40000 ALTER TABLE `events` DISABLE KEYS */;
+/*!40000 ALTER TABLE `events` ENABLE KEYS */;
+
+
 -- Dumping structure for table dusal.migrations
-DROP TABLE IF EXISTS `migrations`;
 CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL
@@ -74,7 +95,6 @@ INSERT INTO `migrations` (`migration`, `batch`) VALUES
 
 
 -- Dumping structure for table dusal.password_resets
-DROP TABLE IF EXISTS `password_resets`;
 CREATE TABLE IF NOT EXISTS `password_resets` (
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -89,31 +109,22 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
 
 
 -- Dumping structure for table dusal.sub_category
-DROP TABLE IF EXISTS `sub_category`;
 CREATE TABLE IF NOT EXISTS `sub_category` (
-  `sub_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `subcat_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `sub` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
-  PRIMARY KEY (`sub_id`)
+  PRIMARY KEY (`subcat_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table dusal.sub_category: ~8 rows (approximately)
+-- Dumping data for table dusal.sub_category: ~3 rows (approximately)
 /*!40000 ALTER TABLE `sub_category` DISABLE KEYS */;
-INSERT INTO `sub_category` (`sub_id`, `sub`) VALUES
-	(1, 'Газарзүй'),
-	(2, 'Биологи'),
-	(3, 'Геологи'),
-	(4, 'Экологи'),
-	(5, 'Хими'),
-	(6, 'Физик'),
-	(7, 'Ус, Цаг уур'),
-	(8, 'Бакалавр'),
-	(9, 'Магистр'),
-	(10, 'Докторант');
+INSERT INTO `sub_category` (`subcat_id`, `sub`) VALUES
+	(1, 'Судалгааны бүтээл'),
+	(2, 'Эрдэм шинжилгээний хурлын илтгэл, эмхтгэл'),
+	(3, 'Лекц');
 /*!40000 ALTER TABLE `sub_category` ENABLE KEYS */;
 
 
 -- Dumping structure for table dusal.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `type` int(10) unsigned NOT NULL DEFAULT '0',
@@ -128,19 +139,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Dumping data for table dusal.users: ~2 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `type`, `image`, `major`, `f_name`, `l_name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-	(1, 1, '571b577f0360abi.jpg', '0', 'Мөнхтулга', 'Сайнбуян', 'monhtulga1122@gmail.com', '$2y$10$usc8T7MiCwDIMSPGohTzcuStOXu1FK4IUGaUxTFi0m5QimMbyFS0q', 'HRAfJF95RuaH5lDm3vISspz3klnJ9KBtgdkdAemYxWV2rjHToKteKdwWszbq', '2016-04-23 15:37:24', '2016-04-25 22:50:23'),
-	(2, 2, 'user.png', '0', 'askjhd', 'kjahsdkajhsd', 'akjshd@kahsgf.com', '$2y$10$7h16J8IP68Ti96rMnW7a4uynv2hhbMgoK9dBCI4YgY9i4fF954wZq', NULL, '2016-04-23 19:07:43', '2016-04-23 19:07:43'),
-	(3, 1, 'user.png', '0', 'akjshd', 'aksjhd', 'kajshdkasd@kajshd.com', '$2y$10$0Rped058hYxz.r1M26AHM.npvkvQ2gf8EpnshV8Unx24NWHiPFvEi', 'dkqvkrIgMJHGpLPApybNHjjlebvtBKfiYAKjniWY03F6AfE6TSc7jtBKlU3v', '2016-04-24 15:37:02', '2016-04-24 15:37:07');
+	(1, 1, '571b577f0360abi.jpg', '0', 'Мөнхтулга', 'Сайнбуян', 'monhtulga1122@gmail.com', '$2y$10$usc8T7MiCwDIMSPGohTzcuStOXu1FK4IUGaUxTFi0m5QimMbyFS0q', '8gS8B5n0Wh8PZouj2aVzHYiODIMCKvtgRiUTWmmPddvu8KqMaimnsjNi4jWi', '2016-04-23 15:37:24', '2016-04-28 05:43:22'),
+	(8, 3, 'user.png', '0', 'Мөнхтулга', 'Сайнбуян', 'tulgaa.2211@yahoo.com', '$2y$10$uCSZRQloGiHLoGIZsP0gVuEyMpWo/HbWXwHEzlScV1zY/vAaSNiPK', NULL, '2016-04-28 05:44:30', '2016-04-28 05:44:30');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 
 -- Dumping structure for table dusal.users_type
-DROP TABLE IF EXISTS `users_type`;
 CREATE TABLE IF NOT EXISTS `users_type` (
   `type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `type_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
